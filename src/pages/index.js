b1de1fe2521fb0d155c1433d6e1eec6143520dda
jsango2 <jure.sango@gmail.com>
2021-02-21 16:25:42 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,9 @@ class RootIndex extends React.Component {
 		return (
 			<>
 				<div>Blog Index</div>
-				<div class='snipcart-summary'>
-					Number of items: <span class='snipcart-total-items'></span>
-					Total price: <span class='snipcart-total-price'></span>
+				<div className='snipcart-summary'>
+					Number of items: <span className='snipcart-total-items'></span>
+					Total price: <span className='snipcart-total-price'></span>
 				</div>
 				{/* <div
 							dangerouslySetInnerHTML={{
@@ -21,17 +21,17 @@ class RootIndex extends React.Component {
 							}}
 						/> */}
 				<button
-					classNmae='snipcart-add-item'
+					className='snipcart-add-item'
 					data-item-id='ZadarNightRun2021'
 					data-item-price='100.00'
 					data-item-url='https://nostalgic-leavitt-ea13f9.netlify.app'
 					data-item-description='Super trka'
-					// data-item-image='https://images.medicaldaily.com/sites/medicaldaily.com/files/2017/02/14/race.jpg'
+					data-item-image='https://images.medicaldaily.com/sites/medicaldaily.com/files/2017/02/14/race.jpg'
 					data-item-name='ZDNIGHTRUN'
 				>
 					Add to cart
 				</button>
-				<button classNmae='snipcart-checkout'>Click here to checkout</button>
+				<button className='snipcart-checkout'>Click here to checkout</button>
 			</>
 		);
 
